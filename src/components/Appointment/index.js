a69f17f6f34c206a3d573c93defdea0c2d881ffe
BreakfastAtKyrils <kyrildr@gmail.com>
2022-06-01 15:47:35 -0400

    add Stories + code for Appointments

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -1,5 +1,9 @@
-import React from "react";
+import React, { Fragment } from 'react'
 import "/Users/kyril-remillard/lighthouse/scheduler/src/components/Appointment/styles.scss";
+import Header from "./Header";
+import Show from "./Show";
+import Empty from "./Empty";
+
 
 export default function Appointment(props) {
   return <article className="appointment"></article>
